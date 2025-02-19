5eefe8ac9c110bb18767e3bde3525401b7a07b7c
Elouan Grimm <elouan.grimm@gmail.com>
2025-02-19 07:29:51 -0800

    added index url for pyodide

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 async function main() {
-    let pyodide = await loadPyodide();
+    let pyodide = await loadPyodide({
+        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.18.1/full/"
+    });
     document.getElementById("run").onclick = async () => {
         let code = document.getElementById("code").value;
         try {
